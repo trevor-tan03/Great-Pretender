4ea8e1896fe5fc9b5c96a4dcdfbb8249815f5497
burgerax5 <93895662+burgerax5@users.noreply.github.com>
2022-05-29 13:36:21 +1200

    Can select characters on characters page
    Using the element's id and indexing the JSON with that id, I am able to retrieve the data needed for the elements in the character-info div. So upon pressing one of the character icons, the description changes to match the selected character.
    
    Still need to edit the JSON file, add images for each of the characters and then fix the sizes of the images.

--- Root/js/characters.js
+++ Root/js/characters.js
@@ -1,18 +1,31 @@
-const info = document.getElementById('info')
+const charPrev = document.getElementById('char-prev');
+const charName = document.getElementById('char-name');
+const charBirth = document.getElementById('char-birth');
+const charDesc = document.getElementById('char-desc');
 
 const onClick = function(){
-    getDetails(this.id)
+    let number = Number(this.id.slice(this.id.indexOf('e')+2)) - 1;
+    getDetails(number);
 }
 
-function getDetails(){
-    const xhttp = new XMLHttpRequest();
-    
+function getDetails(char_id){
+    fetch('./js/details.json')
+        .then(res => res.json())
+        .then(function(data){
+            let first_name = data.details[char_id].name
+            if(first_name.indexOf(' ') === -1){
+                charPrev.src = "./img/character_previews/" + first_name.slice(0).toLowerCase() + '.jpg';
+            }
+            else{
+                charPrev.src = "./img/character_previews/" + first_name.slice(0, first_name.indexOf(' ')).toLowerCase() + '.jpg';
+            }
+            
+            charName.innerHTML = '<h2>' + data.details[char_id].name + '</h2>';
+            charBirth.innerHTML = '<p><b>' + 'Place of birth: ' + '</b>' + data.details[char_id].birth + '</p>';
+            charDesc.innerHTML = '<p>' + data.details[char_id].desc + '<p>';
+        })
 }
 
 for(let i = 1; i < 26; i++){
     document.getElementById('character'+i.toString()).onclick = onClick;
-}
-
-function changeDetails() {
-
 }
